7c94e8298b70dc3e67bbbeaf76dbe79c26e5324b
Thomas Lu <thomas.luqiyu@gmail.com>
2025-01-21 10:38:34 +0100

    update

--- server_java/src/main/java/org/example/character/Gegner.java
+++ server_java/src/main/java/org/example/character/Gegner.java
@@ -3,7 +3,6 @@ package org.example.character;
 
 import org.example.util.Vector2;
 
-import java.util.ArrayList;
 import java.util.UUID;
 
 public class Gegner {
@@ -24,13 +23,48 @@ public class Gegner {
         this.position = new Vector2(startPosition);
         this.type = type;
     }
-    public void update()
-    {
 
+    public Player findPlayer(HashMap allPlayers){
+
+        Player closestPlayer = null;
+        float shortestDistance = Float.MAX_VALUE;
+        for (int i = 0; i < allPlayers.length; i++){
+            Player player = allPlayers.get (i);
+            float distance = position.dst (player.getPosition());
+
+            if (distance < shortestDistance){
+                shortestDistance = distance;
+                closestPlayer = player;
+            }
+        }
+        return closestPlayer;
     }
+
+    public void moveTowards (Player playerPosition, float delta) {
+        Vector2 direction = playerPosition.sub (position);
+        direction = direction.nor();
+        Vector2 movement = direction.scl (20);
+        position.add (movement);
+    }
+
+    public void update(float delta) {
+        Player closestPlayer = findPlayer(allPlayers);
+        float distance = position.dst (closestPlayer) ;
+        if (distance <= attackRange) {
+            performAttack(playerPosition);
+        } else { moveTowards (closestPlayer, delta);
+        }
+    }
+
+    public void performAttack(Player player) {
+        player.takeDamage (5);
+    }
+
+
     @Override
     public String toString() {
         return "NPC: " + name;
     }
-
 }
+
+
