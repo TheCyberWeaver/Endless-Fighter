61ac35194d204fa1246dcd59cd3268d379dd5dfd
tamara711 <tb.selow@web.de>
2025-01-22 08:18:07 +0100

    Update Apple.java

--- core/src/main/java/io/github/infotest/item/Apple.java
+++ core/src/main/java/io/github/infotest/item/Apple.java
@@ -4,10 +4,17 @@ import io.github.infotest.util.MyAssetManager;
 
 public class Apple extends Item {
     public String color;
+    private int healthRecovery;
 
-    public Apple(MyAssetManager assetManager) {
+    public Apple(MyAssetManager assetManager, int healthRecovery) {
         super("apple","just an apple", assetManager.getLoadingScreenTexture());
+        this healthRecovery = healthRecovery;
     }
+
+    public void eat (Actor actor){
+        actor.heal (healthRecovery);
+    }
+    
     public void render(){
 
     }
