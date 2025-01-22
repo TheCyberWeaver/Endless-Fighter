790548247c49fa6796d7b7e63062bd5792cb47b4
tamara711 <tb.selow@web.de>
2025-01-22 08:25:15 +0100

    Create Coffee.java

---
+++ core/src/main/java/io/github/infotest/item/Coffee.java
@@ -0,0 +1,21 @@
+package io.github.infotest.item;
+
+import io.github.infotest.util.MyAssetManager;
+
+public class Coffee extends Item {
+    public String color;
+    private int ausdauerRecovery;
+
+    public Apple(MyAssetManager assetManager, int ausdauerRecovery) {
+        super("apple","just an apple", assetManager.getLoadingScreenTexture());
+        this ausdauerRecovery = ausdauerRecovery;
+    }
+
+    public void drink (Player player){
+        
+    }
+    
+    public void render(){
+
+    }
+}
