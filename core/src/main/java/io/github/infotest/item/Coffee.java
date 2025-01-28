0224a31e00d286a6b5727f07c263d33fe4aac2d5
tamara711 <tb.selow@web.de>
2025-01-28 09:25:52 +0100

    Update Coffee.java

--- core/src/main/java/io/github/infotest/item/Coffee.java
+++ core/src/main/java/io/github/infotest/item/Coffee.java
@@ -1,21 +1,20 @@
-//package io.github.infotest.item;
-//
-//import io.github.infotest.util.MyAssetManager;
-//
-//public class Coffee extends Item {
-//    public String color;
-//    private float ausdauerRecovery;
-//
-//    public Apple(MyAssetManager assetManager, float ausdauerRecovery) {
-//        super("apple","just an apple", assetManager.getLoadingScreenTexture());
-//        this ausdauerRecovery = ausdauerRecovery;
-//    }
-//
-//    public float getAusdauerRecovery(){
-//        return ausdauerRecovery;
-//    }
-//
-//    public void render(){
-//
-//    }
-//}
+package io.github.infotest.item;
+
+import io.github.infotest.util.MyAssetManager;
+
+public class Coffee extends Item {
+    public String color;
+    private float ausdauerRecovery;
+
+    public Coffee (MyAssetManager assetManager, float ausdauerRecovery) {
+        super("coffee","just an coffee", assetManager.getLoadingScreenTexture());
+        this ausdauerRecovery = ausdauerRecovery;
+    }
+
+    public float getAusdauerRecovery(){
+        return ausdauerRecovery;
+    }
+
+    public void render(){
+    }
+}
