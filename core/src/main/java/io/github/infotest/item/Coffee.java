85d8c393f8180cfe57a2a43ee9500d26a793b8b3
tamara711 <tb.selow@web.de>
2025-01-28 09:55:13 +0100

    Update Coffee.java

--- core/src/main/java/io/github/infotest/item/Coffee.java
+++ core/src/main/java/io/github/infotest/item/Coffee.java
@@ -4,16 +4,15 @@ import io.github.infotest.util.MyAssetManager;
 
 public class Coffee extends Item {
     public String color;
-    private float ausdauerRegen;
+    
 
     public Coffee (MyAssetManager assetManager, float ausdauerRegen) {
         super("coffee","just a coffee", assetManager.getLoadingScreenTexture());
-        this ausdauerRegen = ausdauerRegen;
         
     }
 
-    public float getAusdauerRegen(){
-        return ausdauerRegen;
+      public void use (Player player){
+        player.setAusdauer (player.getMaxAusdauer());
     }
 
     public void render(){
