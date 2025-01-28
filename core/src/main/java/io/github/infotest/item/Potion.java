9dbe3994bbf5684c6c555f5f98018ec489b7d23c
tamara711 <tb.selow@web.de>
2025-01-28 09:54:24 +0100

    Update Potion.java

--- core/src/main/java/io/github/infotest/item/Potion.java
+++ core/src/main/java/io/github/infotest/item/Potion.java
@@ -4,21 +4,13 @@ import io.github.infotest.util.MyAssetManager;
 
 public class Potion extends Item {
     public String color;
-    private float manaRegen;
-    private float duration;
 
     public Potion(MyAssetManager assetManager, float manaRegen, float duration) {
         super("potion","just a potion", assetManager.getLoadingScreenTexture());
-        this.manaRegen = manaRegen;
-        this.duration = duration;
     }
 
-    public float getManaRegen(){
-        return manaRegen;
-    }
-
-    public float getDuration(){
-        return duration;
+    public void use (Player player){
+        player.setMana (player.getMaxMana());
     }
 
     public void render(){
